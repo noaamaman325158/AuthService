d9b0251e4503e379af1a1bd5dad6998ee1a0a7cd
noaa <noaamaman325158@gmail.com>
2023-11-04 00:53:07 +0200

    After building proper docker file for the service of the authentication.

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-import jwt, datetime, os
+import datetime
+import os
+import jwt
 from flask import Flask, request
 from flask_mysqldb import MySQL
 
@@ -24,3 +26,36 @@ def login():
     if not auth:
         return "Missing credentials", 401
     # Check db for username and password
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+        "SELECT email, password FROM user WHERE email=%s", (auth.username,)
+    )
+
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email or auth.password != password:
+            return "Invalid Credentials"
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    else:
+        return "Invalid Credentials", 401
+
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.datetime.utc) + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz
+        },
+        secret,
+        algorithm="HS256",
+    )
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000)
